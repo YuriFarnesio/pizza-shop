b7ddf55ecd17f72c1abc3872a1cae7bb94231697
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-06-12 12:42:13 -0300

    feat: add get daily revenue in period filter mock

--- src/api/mocks/get-daily-revenue-in-period-mock.ts
+++ src/api/mocks/get-daily-revenue-in-period-mock.ts
@@ -1,19 +1,43 @@
+import { addDays, format, isWithinInterval, subDays } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { http, HttpResponse } from 'msw'
 
 import { GetDailyRevenueInPeriodResponse } from '../get-daily-revenue-in-period'
+
+const dailyRevenues: GetDailyRevenueInPeriodResponse = Array.from({
+  length: 60,
+}).map((_, i) => {
+  return {
+    date: format(subDays(new Date(), 60 - 1 - i), 'dd/MM/yyyy', {
+      locale: ptBR,
+    }),
+    receipt: Math.random() * 1000 * (i / 3),
+  }
+})
 
 export const getDailyRevenueInPeriodMock = http.get<
   never,
   never,
   GetDailyRevenueInPeriodResponse
->('/metrics/daily-receipt-in-period', () => {
-  return HttpResponse.json([
-    { date: '01/01/2024', receipt: 2000 },
-    { date: '02/01/2024', receipt: 800 },
-    { date: '03/01/2024', receipt: 8000 },
-    { date: '04/01/2024', receipt: 540 },
-    { date: '05/01/2024', receipt: 400 },
-    { date: '06/01/2024', receipt: 700 },
-    { date: '07/01/2024', receipt: 1000 },
-  ])
+>('/metrics/daily-receipt-in-period', ({ request }) => {
+  const { searchParams } = new URL(request.url)
+
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+
+  let filteredDailyRevenues = dailyRevenues
+
+  const startDate = from ? new Date(from) : subDays(new Date(), 7)
+  const endDate = to ? new Date(to) : from ? addDays(startDate, 7) : new Date()
+
+  filteredDailyRevenues = filteredDailyRevenues.filter((dailyRevenue) => {
+    const [day, month, year] = dailyRevenue.date.split('/')
+
+    return isWithinInterval(new Date(`${year}-${month}-${day}`), {
+      start: subDays(startDate, 1),
+      end: endDate,
+    })
+  })
+
+  return HttpResponse.json(filteredDailyRevenues)
 })
